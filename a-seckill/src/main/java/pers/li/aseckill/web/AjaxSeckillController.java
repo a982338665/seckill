4527e5d1774924b209c39d31a635ee12f4e1ce35
a982338665 <luofengli123@163.com>
2019-05-27 19:39:52 +0800

    1.秒杀商品数据初始化：商品-库存 String，Integer

--- a-seckill/src/main/java/pers/li/aseckill/web/AjaxSeckillController.java
+++ a-seckill/src/main/java/pers/li/aseckill/web/AjaxSeckillController.java
@@ -1,5 +1,6 @@
 package pers.li.aseckill.web;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import pers.li.aseckill.entity.SOrderInfo;
 import pers.li.aseckill.entity.SOrderSeckill;
 import pers.li.aseckill.entity.SUser;
+import pers.li.aseckill.redis.RedisService;
+import pers.li.aseckill.redis.service.GoodsKey;
 import pers.li.aseckill.result.CodeMsg;
 import pers.li.aseckill.result.Result;
 import pers.li.aseckill.service.SGoodService;
@@ -17,47 +20,70 @@ import pers.li.aseckill.service.SOrderService;
 import pers.li.aseckill.service.SeckillService;
 import pers.li.aseckill.vo.SGoodsVo;
 
+import java.util.List;
+
 /**
  * 秒杀静态化：数据传输使用json，亲后端交互使用ajax
  */
 @Controller
 @RequestMapping("/seckill")
-public class AjaxSeckillController {
+public class AjaxSeckillController implements InitializingBean {
 
-	@Autowired
-	SGoodService sGoodService;
-	@Autowired
-	SOrderService sOrderService;
-	@Autowired
-	SeckillService seckillService;
+    @Autowired
+    SGoodService sGoodService;
+    @Autowired
+    SOrderService sOrderService;
+    @Autowired
+    SeckillService seckillService;
+
+    @Autowired
+    RedisService redisService;
     /**
      * QPS:1306
      * 5000 * 10
      * */
     /**
-     *  GET POST有什么区别？
-     * */
-    @RequestMapping(value="/seckill", method= RequestMethod.POST)
+     * GET POST有什么区别？
+     */
+    @RequestMapping(value = "/seckill", method = RequestMethod.POST)
     @ResponseBody
-    public Result<SOrderInfo> miaosha(Model model,SUser user,
-                                     @RequestParam("goodsId")long goodsId) {
+    public Result<SOrderInfo> miaosha(Model model, SUser user,
+                                      @RequestParam("goodsId") long goodsId) {
         model.addAttribute("user", user);
-        if(user == null) {
+        if (user == null) {
             return Result.error(CodeMsg.SESSION_ERROR);
         }
         //判断库存//10个商品，req1 req2
         SGoodsVo goods = sGoodService.getGoodsVoByGoodsId(goodsId);
         int stock = goods.getGoodsStock();
-        if(stock <= 0) {
+        if (stock <= 0) {
             return Result.error(CodeMsg.MIAO_SHA_OVER);
         }
         //判断是否已经秒杀到了
-        SOrderSeckill orderSeckill=sOrderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
-        if(orderSeckill != null) {
+        SOrderSeckill orderSeckill = sOrderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
+        if (orderSeckill != null) {
             return Result.error(CodeMsg.REPEATE_MIAOSHA);
         }
         //减库存 下订单 写入秒杀订单
         SOrderInfo orderInfo = seckillService.seckill(user, goods);
         return Result.success(orderInfo);
+    }
+
+    /**
+     * 容器启动后的回调方法 --> 即系统初始化数据
+     *
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        List<SGoodsVo> sGoodsVos = sGoodService.listSGoodsVo();
+        if (sGoodsVos == null) {
+            return;
+        }
+        for (SGoodsVo sGoodsVo : sGoodsVos
+                ) {
+//            存储秒杀商品库存
+            redisService.set(GoodsKey.getSeckillGoodsStock,""+ sGoodsVo.getId(), sGoodsVo.getSeckillStock());
+        }
     }
 }
